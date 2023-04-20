bc4c0b417de8fc9a9e3a3d7903b098733e06fe61
Tony Kim <wukelong123@gmail.com>
2023-04-20 22:29:34 +0800

    0.75 for control

--- app.py
+++ app.py
@@ -511,7 +511,7 @@ class StableDiffusionInpaint:
                     controlnet_conditioning_image=canny_img.resize((process_width, process_height)),
                     width=process_width,
                     height=process_height,
-                    controlnet_conditioning_scale=0.45,
+                    controlnet_conditioning_scale=0.7,
                     **extra_kwargs,
                 )["images"]
         return images
