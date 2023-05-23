41c862485e78f4e7f93a8da35f8b4dbd1aed9bd5
Tony Kim <wukelong123@gmail.com>
2023-05-23 10:36:47 +0800

    offload lora

--- app.py
+++ app.py
@@ -417,7 +417,7 @@ class StableDiffusionInpaint:
                     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
                     inpaint = StableDiffusionControlNetInpaintPipeline.from_pretrained(model_name, vae=vae, torch_dtype=torch.float16, controlnet=controlnet, safety_checker=None)
                     lora_name = "/home/ubuntu/lora/simple_background_v2.safetensors"
-                    inpaint = lora.load_lora_weights(inpaint, lora_name, 0.5, 'cuda', torch.float16)
+                    # inpaint = lora.load_lora_weights(inpaint, lora_name, 0.5, 'cuda', torch.float16)
                     # lora_name = "/home/ubuntu/lora/" + "FilmProvia2.safetensors"
                     # inpaint = lora.load_lora_weights(inpaint, lora_name, 0.5, 'cuda', torch.float16)
                     print('use control net inpaint pipline - cuda with model:', model_name)
